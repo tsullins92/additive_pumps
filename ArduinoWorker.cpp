842d8935b8f5877c39c5265d43f78e8c5c138eec
tsullins92 <tsullins92@gmail.com>
2017-07-11 21:43:05 -0500

    serial finally seems to work. Fixed issue of arduino serial monitor needing to be open when the program was running.

--- ArduinoWorker.cpp
+++ ArduinoWorker.cpp
@@ -9,6 +9,13 @@
 #include "BufferedAsyncSerial.h"
 #include <sstream>
 #include <chrono>
+#include <fcntl.h>
+#include <termios.h>
+#include <string.h> // needed for memset
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 ArduinoWorker::ArduinoWorker() :
   m_Mutex(),
@@ -31,18 +38,43 @@ void ArduinoWorker::set_data(string* pump_command)
 
 void ArduinoWorker::do_work(FrmMain* caller)
 {    
-    char serialPortFilename[] = "/dev/ttyACM0";  
+    struct termios tio;
+    struct termios stdio;
+    int tty_fd;
+    unsigned char c='D';
+    fd_set rdset;
+
+    memset(&stdio,0,sizeof(stdio));
+    stdio.c_iflag=0;
+    stdio.c_oflag=0;
+    stdio.c_cflag=0;
+    stdio.c_lflag=0;
+    stdio.c_cc[VMIN]=1;
+    stdio.c_cc[VTIME]=0;
+    tcsetattr(STDOUT_FILENO,TCSANOW,&stdio);
+    tcsetattr(STDOUT_FILENO,TCSAFLUSH,&stdio);
+    fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);       // make the reads non-blocking
+
+    memset(&tio,0,sizeof(tio));
+    tio.c_iflag=0;
+    tio.c_oflag=0;
+    tio.c_cflag=CS8|CREAD|CLOCAL;           // 8n1, see termios.h for more information
+    tio.c_lflag=0;
+    tio.c_cc[VMIN]=1;
+    tio.c_cc[VTIME]=5;
+
+    tty_fd=open("/dev/ttyACM0", O_RDWR | O_NONBLOCK);      
+    cfsetospeed(&tio,B9600);            // 9600 baud
+    cfsetispeed(&tio,B9600);            // 9600 baud
+
+    tcsetattr(tty_fd,TCSANOW,&tio);
     {
         std::lock_guard<std::mutex> lock(m_Mutex);
         m_has_stopped = false;
     }
     for(;;)
     {    
-        FILE *serPort = fopen(serialPortFilename, "w");
-        if (serPort == NULL)
-		{
-			printf("ERROR");
-		}
+        
         //sleep to give time for serial to buffer and main thread to perform set_data()
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
                
@@ -52,17 +84,17 @@ void ArduinoWorker::do_work(FrmMain* caller)
             
             if (m_shall_stop)
             {
-                char msg[] = {'h'};
-                fwrite(msg, sizeof(char), sizeof(msg), serPort); 
+                c = {'l'};
+                write(tty_fd,&c,1);
                 break;           
             }
             
-            char msg[] = {m_pump_command[0]};
-            fwrite(msg, sizeof(char), sizeof(msg), serPort); 
+            c = {m_pump_command[0]};
+            write(tty_fd,&c,1);       
             cout<<"m_pump_command = "<<m_pump_command<<endl;  
-            fclose(serPort);
         }
     }
+    close(tty_fd);
     {
     std::lock_guard<std::mutex> lock(m_Mutex);
     m_shall_stop = false;
