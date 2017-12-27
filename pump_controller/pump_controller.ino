2fa9917259b5c108791bb5b81984d41b45d0cf24
SULLINS <TSSULL1@monsanto.com>
2017-12-27 14:00:58 -0600

    pump_controller.ino updated to control all ten pumps instead of just 2

--- pump_controller/pump_controller.ino
+++ pump_controller/pump_controller.ino
@@ -1,5 +1,13 @@
+#define PUMP1 1    //pin for turning transistor on/offz
+#define PUMP2 2    //pin for turning transistor on/offz
+#define PUMP3 3    //pin for turning transistor on/offz
+#define PUMP4 4    //pin for turning transistor on/offz
 #define PUMP5 5    //pin for turning transistor on/offz
 #define PUMP6 6    //pin for turning transistor on/offz
+#define PUMP7 7    //pin for turning transistor on/offz
+#define PUMP8 8    //pin for turning transistor on/offz
+#define PUMP9 9    //pin for turning transistor on/offz
+#define PUMP10 10    //pin for turning transistor on/offz
 const byte numChars = 2;
 char msg[numChars];
 
@@ -9,8 +17,16 @@ void setup()
 {
   Serial.begin(9600,SERIAL_8N1);   // Debugging only
   Serial.println(msg[0]);
+  pinMode(PUMP1, OUTPUT);
+  pinMode(PUMP2, OUTPUT);
+  pinMode(PUMP3, OUTPUT);
+  pinMode(PUMP4, OUTPUT);
   pinMode(PUMP5, OUTPUT);
   pinMode(PUMP6, OUTPUT);
+  pinMode(PUMP7, OUTPUT);
+  pinMode(PUMP8, OUTPUT);
+  pinMode(PUMP9, OUTPUT);
+  pinMode(PUMP10, OUTPUT);
 }
 
 
@@ -58,32 +74,40 @@ void command_pumps(){
    Serial.println(msg[1]); 
    if (msg[0] == 'l'||msg[1]=='l')
   {  
+    digitalWrite(PUMP1, LOW);  
+    digitalWrite(PUMP2, LOW); 
+    digitalWrite(PUMP3, LOW);  
+    digitalWrite(PUMP4, LOW); 
     digitalWrite(PUMP5, LOW);  
-    digitalWrite(PUMP6, LOW);       
+    digitalWrite(PUMP6, LOW); 
+    digitalWrite(PUMP7, LOW);  
+    digitalWrite(PUMP8, LOW); 
+    digitalWrite(PUMP9, LOW);  
+    digitalWrite(PUMP10, LOW);       
   } 
   else if (msg[1] == 'm')
   {
      if(msg[0]=='0'){
-      digitalWrite(PUMP5, HIGH);
+      digitalWrite(PUMP1, HIGH);
       delay(50);
-      digitalWrite(PUMP5, LOW);
+      digitalWrite(PUMP1, LOW);
       delay(800);
      }           
     else if (msg[0]=='1'){
-      digitalWrite(PUMP6, HIGH);
+      digitalWrite(PUMP2, HIGH);
       delay(50);
-      digitalWrite(PUMP6, LOW);
+      digitalWrite(PUMP2, LOW);
       delay(800);
      }
     else if (msg[0]=='2'){
-      digitalWrite(PUMP5, HIGH); 
+      digitalWrite(PUMP3, HIGH); 
       delay(20);
-      digitalWrite(PUMP5, LOW);
+      digitalWrite(PUMP3, LOW);
      }
     else if (msg[0]=='3'){
-      digitalWrite(PUMP5, HIGH); 
+      digitalWrite(PUMP4, HIGH); 
       delay(20);
-      digitalWrite(PUMP5, LOW);
+      digitalWrite(PUMP4, LOW);
      }
     else if (msg[0]=='4'){
       digitalWrite(PUMP5, HIGH); 
@@ -91,55 +115,56 @@ void command_pumps(){
       digitalWrite(PUMP5, LOW);
      }
     else if (msg[0]=='5'){
-      digitalWrite(PUMP5, HIGH); 
+      digitalWrite(PUMP6, HIGH); 
       delay(20);
-      digitalWrite(PUMP5, LOW);
+      digitalWrite(PUMP6, LOW);
      }
     else if (msg[0]=='6'){
-      digitalWrite(PUMP5, HIGH); 
+      digitalWrite(PUMP7, HIGH); 
       delay(20);
-      digitalWrite(PUMP5, LOW);
+      digitalWrite(PUMP7, LOW);
      }
     else if (msg[0]=='7'){
-      digitalWrite(PUMP5, HIGH); 
+      digitalWrite(PUMP8, HIGH); 
       delay(20);
-      digitalWrite(PUMP5, LOW);
+      digitalWrite(PUMP8, LOW);
      }
     else if (msg[0]=='8'){
-      digitalWrite(PUMP5, HIGH); 
+      digitalWrite(PUMP9, HIGH); 
       delay(20);
-      digitalWrite(PUMP5, LOW);
+      digitalWrite(PUMP9, LOW);
      }
     else if (msg[0]=='9'){
-      digitalWrite(PUMP5, HIGH); 
+      digitalWrite(PUMP10, HIGH); 
       delay(20);
-      digitalWrite(PUMP5, LOW);
+      digitalWrite(PUMP10, LOW);
      }
   }
   else if (msg[1] == 'h')
   {
     if(msg[0]=='0')  
-      digitalWrite(PUMP5, HIGH);
+      digitalWrite(PUMP1, HIGH);
       //Serial.println("HI");             
     else if (msg[0]=='1')
-      digitalWrite(PUMP6, HIGH);
+      digitalWrite(PUMP2, HIGH);
     else if (msg[0]=='2')
-      digitalWrite(PUMP5, HIGH); 
+      digitalWrite(PUMP3, HIGH); 
     else if (msg[0]=='3')
-      digitalWrite(PUMP5, HIGH); 
+      digitalWrite(PUMP4, HIGH); 
     else if (msg[0]=='4')
       digitalWrite(PUMP5, HIGH); 
     else if (msg[0]=='5')
-      digitalWrite(PUMP5, HIGH); 
+      digitalWrite(PUMP6, HIGH); 
     else if (msg[0]=='6')
-      digitalWrite(PUMP5, HIGH); 
+      digitalWrite(PUMP7, HIGH); 
     else if (msg[0]=='7')
-      digitalWrite(PUMP5, HIGH); 
+      digitalWrite(PUMP8, HIGH); 
     else if (msg[0]=='8')
-      digitalWrite(PUMP5, HIGH); 
+      digitalWrite(PUMP9, HIGH); 
     else if (msg[0]=='9')
-      digitalWrite(PUMP5, HIGH); 
+      digitalWrite(PUMP10, HIGH); 
   }
   return;
 }
+
 
